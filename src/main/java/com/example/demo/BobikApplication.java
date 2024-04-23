ddfd7f086a431fe000ede6bd0b323856c041d383
bobik <oliferchukrivne@i.ua>
2024-04-23 16:59:16 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/BobikApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobikApplication.class, args);
+	}
+
+}
