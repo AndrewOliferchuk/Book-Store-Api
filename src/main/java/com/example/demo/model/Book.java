feef851b54e390bb4a84aa34c5a1dd9615387a57
bobik <oliferchukrivne@i.ua>
2024-04-24 16:14:30 +0300

    task done2.0

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,9 +14,12 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String author;
     private String isbn;
+    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     private String coverImage;
